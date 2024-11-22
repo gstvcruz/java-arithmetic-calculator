ee8b652ffb44858d8fce75c534a68c8cec6af40d
Gustavo Cruz <gustavoindeolcruz@gmail.com>
2024-11-22 18:57:32 -0300

    add stack and queue of linked list

--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) throws Exception {
+    Scanner scanner = new Scanner(System.in);
+
+    while (true) {
+      System.out.print("Enter expression (type 'exit' to quit): ");
+      String input = scanner.nextLine();
+
+      if ("exit".equalsIgnoreCase(input)) {
+        System.out.println("Goodbye!");
+        break;
+      }
+
+      System.out.println("Result: ");
+    }
+
+    scanner.close();
     NotationConverter nc = new NotationConverter(args[0]);
     StringQueue postFixExp = nc.getPostFixNotation();
     Calculator calculator = new Calculator(postFixExp);
